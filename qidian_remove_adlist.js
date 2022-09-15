58bd1ec7823259ec764ba961f0c1dba7c8fdc675
Larkin <mail@larkin.wang>
2022-09-15 10:53:49 +0800

    fix qidian_remove_adlist

--- qidian_remove_adlist.js
+++ qidian_remove_adlist.js
@@ -7,7 +7,7 @@ $done({body});
 
 /**********************************************************
 [Script]
-http-response https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch\?bookId=\d*&positions=.* script-path=https://raw.githubusercontent.com/LarkinZero/Surge/master/qidian_remove_adlist.js,requires-body=true
+http-response https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch\?bookId=\d*&positions=iosnewlastpage script-path=https://raw.githubusercontent.com/LarkinZero/Surge/master/qidian_remove_adlist.js,requires-body=true
 [MITM]
 hostname = *.qidian.com
 **********************************************************/
